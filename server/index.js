36afdc38b9846672f5b9e289f98a47621d3751a8
Vaigai Vendhan <133666759+vaigai138@users.noreply.github.com>
2024-08-02 20:33:13 +0530

    server updated

--- server/index.js
+++ server/index.js
@@ -6,17 +6,20 @@ const path = require("path");
 let users = require("./sample.json");
 
 const app = express();
-const port =process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     methods: ["GET", "POST", "PATCH", "DELETE"],
 }));
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Get all users
 app.get("/users", (req, res) => {
-    return res.json(users);
+    res.json(users);
 });
 
 // Delete a user
@@ -28,7 +31,7 @@ app.delete("/users/:id", (req, res) => {
         if (err) {
             return res.status(500).json({ message: "Error writing file" });
         }
-        return res.json(users);
+        res.json(users);
     });
 });
 
@@ -47,7 +50,7 @@ app.post("/users", (req, res) => {
         if (err) {
             return res.status(500).json({ message: "Error writing file" });
         }
-        return res.json(users);
+        res.json(users);
     });
 });
 
@@ -72,8 +75,13 @@ app.patch("/users/:id", (req, res) => {
         if (err) {
             return res.status(500).json({ message: "Error writing file" });
         }
-        return res.json(users);  // Return the updated list of users
+        res.json(users);
     });
+});
+
+// Handle any other requests by serving the React app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
